36031fc5b4611d0e9e5eb4be00cfe0885b0f3489
Kallol Hazarika <Kallol.Hazarika@mu-sigma.com>
2025-06-12 17:26:15 +0530

    initial codes for the wordle copy app

---
+++ app/Components/InputArr.jsx
@@ -0,0 +1,170 @@
+import { useState, useEffect, useRef } from "react"
+import styles from "../lib/css/inputarr.module.css"
+import axios from "axios"
+import toast, { Toaster } from "react-hot-toast"
+import { count } from "../lib/utils/utils"
+
+export default function InputArr({row, rowNum, setRowNum, elems, ref, answer, classNames, setClassNames, setSuccess, setModalOpen}){
+
+    // console.log(rowNum)
+    const [attempt, setAttempt] = useState("")
+    
+    
+    const rowElems = elems.map(( elem, index ) => 
+        <input 
+            key={elem}
+            ref={(reference) => ref.current[elem] = reference}
+            className={`${styles.input} ${styles[classNames[row][index]]} `}  
+            maxLength={1}
+            onKeyUp={(e) => handleBackandEnter(e, row, index)}
+            onChange={(e) => handleChange(e, row, index)}
+            value = {attempt[index] ? attempt[index] : ""}
+            />
+    )
+    
+    useEffect(() => {
+        ref?.current[0].focus()
+    },[])
+    // console.log(ref.current)
+
+    const handleChange = (e, row, index) => {
+            console.log("index", index)
+            setAttempt(prev => prev + e.target.value.toUpperCase())
+            if(index !== 4){
+                ref.current[(rowNum * 5) + (index + 1)].focus()
+            }
+        }
+    
+        
+    const handleBackandEnter = async (e, row, index) => {
+        
+
+        if(e.key === "Backspace") {
+            setAttempt(prev => prev.substring(0, prev.length - 1))
+            if(index === 4) {
+                ref.current[(rowNum * 5) + (index)].focus()
+            }else if(index !== 0){
+                ref.current[(rowNum * 5) + (index - 1)].focus()
+            }
+
+        }
+
+        console.log(answer, attempt)
+        
+        if(e.key === "Enter"){
+            if(attempt.length === 5){
+                try {
+                    const res = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${attempt}`)
+                    let map = new Map()
+                    for(let i = 0; i < attempt.length; i++) {
+                        if(answer.has(attempt[i]) && answer.get(attempt[i]).includes(i)){
+                            // console.log(attempt[i], i)
+                            // setAnswerCopy(prev => {
+                            //     let ans = new Map(prev)
+                            //     ans.set(attempt[i], [...ans.get(attempt[i]).filter(x => x !== i)])
+                            //     return ans
+                            // }) 
+                            if(map.has(attempt[i])){
+                                map.set(attempt[i], map.get(attempt[i]) + 1)
+                            }else{
+                                map.set(attempt[i], 1)
+                            }
+
+                            // console.log(map)
+                            
+                            setClassNames(prev => {
+                                const result = [...prev];
+                                result[row] = [...result[row]]; // Create a shallow copy of the row array
+                                result[row][i] = "inputCorrect";
+                                return result;
+                            })
+                        }
+                    }
+
+                    // console.log(map)
+                    
+                    for(let i = 0; i < attempt.length; i++) {
+                        if(answer.has(attempt[i])) {
+                                if(map.has(attempt[i])) {
+                                    if(answer.get(attempt[i]).length - map.get(attempt[i]) > 0) {
+                                        setClassNames(prev => {
+                                            const result = [...prev];
+                                            result[row] = [...result[row]]; // Create a shallow copy of the row array
+                                            result[row][i] = result[row][i] === "inputDefault" ?  "inputIncorrectPlace" : result[row][i];
+                                            // result[row][i] === "inputDefault" ?  "inputIncorrectPlace" : result[row][i];
+                                            return result;
+                                        })
+                                        map.set(attempt[i], map.get(attempt[i]) + 1 )
+                                    }
+                                }else{
+                                    setClassNames(prev => {
+                                        const result = [...prev];
+                                        result[row] = [...result[row]]; // Create a shallow copy of the row array
+                                        result[row][i] = result[row][i] === "inputDefault" ?  "inputIncorrectPlace" : result[row][i];
+                                        // result[row][i] === "inputDefault" ?  "inputIncorrectPlace" : result[row][i];
+                                        return result;
+                                    })
+                                }
+                            }
+                    }
+
+                    setClassNames(prev => {
+                        const result = [...prev]
+                        result[row] = result[row].map(classname => classname === "inputDefault" ? "inputNotPresent" : classname)
+                        return result
+                    }) 
+                    
+                    
+
+                    if(rowNum !== 5){
+                        setRowNum(prev => prev + 1)
+                        ref.current[(rowNum * 5) + (index + 1)].focus()
+                    }
+                    
+                    // console.log(ref.current)
+                }catch(err) {
+                    if(err.response.data.title === 'No Definitions Found') {
+                        toast.error("Please enter a valid word")
+                    }
+                }
+            }else{
+                toast.error("Please try with a five letter word")
+            }
+
+            // console.log(answer.get("answer"))
+
+            if(attempt === answer.get("ans")) {
+                if(rowNum === 5) {
+                    toast("Phew")
+                }else if(rowNum === 0 || rowNum === 1) {
+                    toast("Awesome!")
+                }else {
+                    toast("Congrats")
+                }
+                // setClassNames(prev => )
+                setClassNames(prev => {
+                    for(let i = rowNum + 1; i < classNames.length; i++) {
+                        console.log(prev[i])
+                        // prev[i] = [...prev[i]]
+                        prev[i] = prev[i].map(x => x === "inputDefault" ? "inputDisabled" : x)
+                    }
+                    return prev
+                })
+            }
+            // if(rowNum === 5 && attempt === answer.get("ans")) {
+            // }
+            if(rowNum === 5 && attempt !== answer.get("ans")) {
+                toast(`The answer is ${answer.get("ans")}. Better luck next time`)
+            }
+        }
+    }
+
+    return (
+        <div key = {row} className="flex justify-center gap-2 p-1">
+           {rowElems}
+            <Toaster 
+                position="bottom-center"
+            />
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,103 +1,148 @@
+"use client"
+import { useActionState, useRef, useState } from "react";
+import InputArr from "./Components/InputArr";
+import { answer } from "./lib/data/data";
+import styles from "./lib/css/main.module.css"
+import Modal from "./Components/Modal";
+import { MdHelpCenter } from "react-icons/md";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  
+  const ref = useRef([])
+  // const [occurence, setOccurence] = useState
+  
+  const answerMap = new Map()
+  answerMap.set("ans", answer)
+  for(let i = 0; i < answer.length; i++) {
+    if(!answerMap.has(answer[i])){
+      answerMap.set(answer[i], [i])
+    }else{
+      answerMap.set(answer[i], [...answerMap.get(answer[i]), i])
+    }
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  // console.log(answerMap)
+
+  const [classNames, setClassNames] = useState(new Array(6).fill(new Array(5).fill("inputDefault")))
+  const [rowNum, setRowNum] = useState(0)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [displayHelp, setDisplayHelp] = useState(false) 
+  const [success, setSuccess] = useState(false)
+
+
+  // console.log(classNames)
+
+  const showHelp = () => {
+    setModalOpen(true)
+    setDisplayHelp(true)
+  }
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.title}>
+        <h1 className={styles.title}>WORDLE</h1>
+        <MdHelpCenter className={styles.help} onClick={showHelp} />
+      </div>
+      <div >
+        <InputArr
+          elems = {[0,1,2,3,4]}
+          classNames = {classNames}
+          setClassNames={setClassNames}
+          row = {0}
+          rowNum={rowNum}
+          setRowNum = {setRowNum}
+          ref = {ref}
+          answer = {answerMap}
+          setSuccess = {setSuccess}
+          setModalOpen = {setModalOpen}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        
+        <InputArr
+          elems = {[5,6,7,8,9]}
+          classNames = {classNames}
+          setClassNames={setClassNames}
+          row = {1}
+          rowNum={rowNum}
+          setRowNum = {setRowNum}
+          ref = {ref}
+          answer = {answerMap}
+          setSuccess = {setSuccess}
+          setModalOpen = {setModalOpen}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+        <InputArr
+          elems = {[10, 11, 12, 13, 14]}
+          classNames = {classNames}
+          setClassNames={setClassNames}
+          row = {2}
+          rowNum={rowNum}
+          setRowNum = {setRowNum}
+          ref = {ref}
+          answer = {answerMap}
+          setSuccess = {setSuccess}
+          setModalOpen = {setModalOpen}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <InputArr
+          elems = {[15, 16, 17, 18, 19]}
+          classNames = {classNames}
+          setClassNames={setClassNames}
+          row = {3}
+          rowNum={rowNum}
+          setRowNum = {setRowNum}
+          ref = {ref}
+          answer = {answerMap}
+          setSuccess = {setSuccess}
+          setModalOpen = {setModalOpen}
+          />
+        <InputArr
+          elems = {[20, 21, 22, 23, 24]}
+          classNames = {classNames}
+          setClassNames={setClassNames}
+          row = {4}
+          rowNum={rowNum}
+          setRowNum = {setRowNum}
+          ref = {ref}
+          answer = {answerMap}
+          setSuccess = {setSuccess}
+          setModalOpen = {setModalOpen}
+          />
+        <InputArr
+          elems = {[25, 26, 27, 28, 29]}
+          classNames = {classNames}
+          setClassNames={setClassNames}
+          row = {5}
+          rowNum={rowNum}
+          setRowNum = {setRowNum}
+          ref = {ref}
+          answer = {answerMap}
+          setSuccess = {setSuccess}
+          setModalOpen = {setModalOpen}
+        />
+      </div>
+      
+      
+      {
+        modalOpen 
+        
+        &&
+
+        <Modal
+          open = {modalOpen}
+          setIsOpen = {setModalOpen}
+          setDisplayHelp = {setDisplayHelp}
+          setSuccess = {setSuccess}
+        >
+          {displayHelp && 
+            <Image src={"/wordle_howtoplay.png"} alt="how to play" width={400} height={100}/>
+          }
+          {/* {success && 
+            <h1>Congrats! </h1>
+          } */}
+          {/* <img src="public/wordle_howtoplay.PNG" alt="hhh" />  */}
+
+        </Modal>
+      }
     </div>
+
   );
 }
